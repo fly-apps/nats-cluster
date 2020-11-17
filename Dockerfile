ab090185881b66b104afdddc4aa164110830e0af
Kurt <mrkurt@gmail.com>
2020-11-16 22:46:54 -0600

    :dancing_pickle:

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
-FROM nats:2.1.9-alpine
-RUN apk add bind-tools
+FROM nats:2.1.9-scratch
 ADD nats.conf /etc/nats.conf
-ADD start.sh /usr/local/bin/
-RUN chmod +x /usr/local/bin/start.sh
-ENTRYPOINT "/usr/local/bin/start.sh"
+
+CMD ["-c", "/etc/nats.conf"]
--- start.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-routes=$(dig aaaa ord.nats-cluster-example.internal @fdaa::3 +short | awk -vORS=, '{ printf "\"nats-route://[%s]:4248\"\n",$1 }' | paste -s -d, -)
-sed -i "s|NATS_ROUTES|$routes|" /etc/nats.conf
-nats-server -c /etc/nats.conf
